ee003d36d066076bb566b9caea8b5c1975cea489
Alexandre Bury <alexandre.bury@gmail.com>
2016-06-25 13:51:48 -0700

    Add update doc script

---
+++ update_doc.sh
@@ -0,0 +1,10 @@
+set -e
+
+cargo doc
+FROM=$(git rev-parse --short HEAD)
+git checkout gh-pages
+rsync -a target/doc/ .
+git add .
+git commit -m "Update doc for ${FROM}"
+# git push
+git checkout master
