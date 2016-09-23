d29f80869d537e85ee5ab755770ebee0a6aecb07
Alexandre Bury <alexandre.bury@gmail.com>
2016-09-22 22:04:55 -0700

    Don't inline doc in prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -6,11 +6,17 @@
 //! use cursive::prelude::*;
 //! ```
 
+#[doc(no_inline)]
 pub use {Cursive, Printer, With};
+#[doc(no_inline)]
 pub use event::{Event, Key};
+#[doc(no_inline)]
 pub use view::{Boxable, Finder, Identifiable, Selector, View};
+#[doc(no_inline)]
 pub use views::{BoxView, Button, Checkbox, Dialog, DummyView, EditView,
                 IdView, KeyEventView, LinearLayout, ListView, Panel,
                 ProgressBar, SelectView, SliderView, TextArea, TextView};
+#[doc(no_inline)]
 pub use vec::Vec2;
+#[doc(no_inline)]
 pub use menu::MenuTree;
