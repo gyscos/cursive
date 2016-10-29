949b1e963293513c3123cc576449dccad13a0cb9
Alexandre Bury <alexandre.bury@gmail.com>
2016-10-28 17:13:53 -0700

    Add tab support to pancurses

--- src/backend/curses/pan.rs
+++ src/backend/curses/pan.rs
@@ -100,9 +100,10 @@ impl backend::Backend for Concrete {
                 pancurses::Input::Character('\n') => Event::Key(Key::Enter),
                 // TODO: wait for a very short delay. If more keys are
                 // pipelined, it may be an escape sequence.
-                pancurses::Input::Character('\u{1b}') => Event::Key(Key::Esc),
                 pancurses::Input::Character('\u{7f}') |
                 pancurses::Input::Character('\u{8}') => Event::Key(Key::Backspace),
+                pancurses::Input::Character('\u{9}') => Event::Key(Key::Tab),
+                pancurses::Input::Character('\u{1b}') => Event::Key(Key::Esc),
                 pancurses::Input::Character(c) if 32 <= (c as u32) &&
                                                   (c as u32) <= 255 => {
                     Event::Char(utf8::read_char(c as u8, || {
