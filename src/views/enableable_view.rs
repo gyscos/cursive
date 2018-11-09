edeb7a8c13c90424bdcf80d315ec18ce5a15032a
Alexandre Bury <alexandre.bury@gmail.com>
2018-11-09 14:19:37 -0800

    Add EnableableView

---
+++ src/views/enableable_view.rs
@@ -0,0 +1,41 @@
+use event::{Event, EventResult};
+use view::{View, ViewWrapper};
+use {Printer, With};
+
+/// Wrapper around another view that can be enabled/disabled at will.
+///
+/// When disabled, all child views will be disabled and will stop receiving events.
+pub struct EnableableView<V> {
+    view: V,
+    enabled: bool,
+}
+
+impl<V> EnableableView<V> {
+    /// Creates a new `EnableableView` around `view`.
+    ///
+    /// It will be enabled by default.
+    pub fn new(view: V) -> Self {
+        EnableableView {
+            view,
+            enabled: true,
+        }
+    }
+
+    impl_enabled!(self.enabled);
+}
+
+impl<V: View> ViewWrapper for EnableableView<V> {
+    wrap_impl!(self.view: V);
+
+    fn wrap_on_event(&mut self, event: Event) -> EventResult {
+        if self.enabled {
+            self.view.on_event(event)
+        } else {
+            EventResult::Ignored
+        }
+    }
+
+    fn wrap_draw(&self, printer: &Printer) {
+        self.view.draw(&printer.enabled(self.enabled));
+    }
+}
