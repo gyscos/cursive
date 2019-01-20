0fa49a1e74c7cecf83e7faa5788958da58bf228d
Andrzej J Skalski <njskalski@google.com>
2019-01-20 12:42:58 +0100

    review1

--- src/backend/puppet/observed.rs
+++ src/backend/puppet/observed.rs
@@ -73,7 +73,7 @@ impl ObservedScreen {
         assert!(index.x < self.size.x);
         assert!(index.y < self.size.y);
 
-        index.y * self.size.x + index.y
+        index.x * self.size.y + index.y
     }
 
     pub fn clear(&mut self, style : &Rc<ObservedStyle>) {
