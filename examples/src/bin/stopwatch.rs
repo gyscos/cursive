2fd22a623a7a74d126f96f79c4f05c90d9ed70a5
Tianyi Shi <ShiTianyi2001@outlook.com>
2020-09-22 19:12:39 +0800

    this won't work

--- examples/src/bin/stopwatch.rs
+++ examples/src/bin/stopwatch.rs
@@ -3,7 +3,7 @@ use cursive::traits::*;
 fn main() {
     let mut siv = cursive::default();
     let timer = Timer::TimerView::new();
-    siv.add_layer(timer.fixed_width(9).with_name("timer"));
+    siv.add_layer(timer.fixed_width(8).with_name("timer"));
     siv.add_layer(cursive::views::Dialog::info(
         "Press 'Space' to start/pause/resume the timer!",
     ));
@@ -14,15 +14,17 @@ fn main() {
 mod Timer {
     use chrono::{DateTime, Duration, Local};
     use cursive::{
-        event::{Event, EventResult},
+        event::{Callback, Event, EventResult, Key},
         view::View,
-        Printer,
+        Cursive, Printer,
     };
+    use std::rc::Rc;
 
     pub struct TimerView {
         elapsed: Duration,
         last_update: DateTime<Local>,
         paused: bool,
+        on_stop: Option<Rc<dyn Fn(&mut Cursive, Duration)>>,
     }
 
     impl TimerView {
@@ -30,6 +32,7 @@ mod Timer {
             Self {
                 elapsed: Duration::zero(),
                 last_update: Local::now(),
+                on_stop: None,
                 paused: true,
             }
         }
@@ -57,6 +60,31 @@ mod Timer {
                 self.elapsed + (Local::now() - self.last_update)
             }
         }
+
+        /// Sets a callback to be used when `<Enter>` is pressed.
+        ///
+        /// Also happens if the user clicks an item.
+        ///
+        /// The elapsed time will be given to the callback.
+        ///
+        /// See also cursive::views::select_view::SelectView::on_submit
+        pub fn on_stop<F, R>(&mut self, cb: F)
+        where
+            F: 'static + Fn(&mut Cursive, Duration) -> R,
+        {
+            self.on_stop = Some(Rc::new(move |s, t| {
+                cb(s, t);
+            }));
+        }
+
+        fn stop(&mut self) -> EventResult {
+            self.pause();
+            let cb = self.on_stop.clone().unwrap();
+            // We return a Callback Rc<|s| cb(s, &*v)>
+            EventResult::Consumed(Some(Callback::from_fn(move |s| {
+                cb(s, self.elapsed)
+            })))
+        }
     }
     impl View for TimerView {
         fn draw(&self, printer: &Printer) {
@@ -69,12 +97,15 @@ mod Timer {
                 Event::Char(' ') => {
                     self.pause_or_resume();
                 }
+                Event::Key(Key::Enter) if self.on_stop.is_some() => {
+                    return self.stop();
+                }
                 _ => return EventResult::Ignored,
             }
             EventResult::Consumed(None)
         }
     }
-    /// pretty-prints a `chrono::Duration`
+    /// pretty-prints a `chrono::Duration` in the form "HH:MM:SS"
     fn pretty(duration: Duration) -> String {
         let s = duration.num_seconds();
         let (h, s) = (s / 3600, s % 3600);
