bad48c4cf20f4319f9d4ae80985a736f02e42d99
Tianyi Shi <ShiTianyi2001@outlook.com>
2020-09-23 00:29:02 +0800

    laps

--- examples/src/bin/stopwatch.rs
+++ examples/src/bin/stopwatch.rs
@@ -16,14 +16,24 @@ mod Timer {
     use cursive::{
         event::{Callback, Event, EventResult, Key},
         view::View,
-        Cursive, Printer,
+        Cursive, Printer, Vec2,
     };
     use std::rc::Rc;
 
+    /// ```ignore
+    ///                  lap    lap          lap
+    /// start       start |      |     start  |
+    ///   o--------x   o-----------x      o-----------x
+    ///          pause           pause            pause(end)
+    /// ```
     pub struct TimerView {
-        elapsed: Duration,
-        last_update: DateTime<Local>,
+        pub elapsed: Duration,
+        pub lap_elapsed: Duration,
         paused: bool,
+        pub pause_moments: Vec<DateTime<Local>>,
+        pub start_moments: Vec<DateTime<Local>>,
+        pub lap_moments: Vec<DateTime<Local>>,
+        pub laps: Vec<Duration>,
         on_stop: Option<Rc<dyn Fn(&mut Cursive, Duration)>>,
     }
 
@@ -31,19 +41,35 @@ mod Timer {
         pub fn new() -> Self {
             Self {
                 elapsed: Duration::zero(),
-                last_update: Local::now(),
+                lap_elapsed: Duration::zero(),
                 on_stop: None,
+                start_moments: Vec::new(),
+                pause_moments: Vec::new(),
+                lap_moments: Vec::new(),
+                laps: Vec::new(),
                 paused: true,
             }
         }
+        pub fn last_start(&self) -> DateTime<Local> {
+            self.start_moments[self.start_moments.len() - 1]
+        }
+        pub fn last_pause(&self) -> DateTime<Local> {
+            self.pause_moments[self.pause_moments.len() - 1]
+        }
+        pub fn last_lap(&self) -> DateTime<Local> {
+            self.lap_moments[self.lap_moments.len() - 1]
+        }
         pub fn pause(&mut self) {
             assert!(self.paused == false, "Already paused!");
-            self.elapsed = self.read();
+            let moment = Local::now();
+            self.pause_moments.push(moment);
+            self.elapsed = self.elapsed + (moment - self.last_start());
+            self.lap_elapsed = self.read_lap_elapsed(moment);
             self.paused = true;
         }
         pub fn resume(&mut self) {
             assert!(self.paused == true, "Already running!");
-            self.last_update = Local::now();
+            self.start_moments.push(Local::now());
             self.paused = false;
         }
         pub fn pause_or_resume(&mut self) {
@@ -53,12 +79,35 @@ mod Timer {
                 self.pause();
             }
         }
+        pub fn lap(&mut self) -> Option<Duration> {
+            // assert!(!self.paused, "Paused!");
+            if self.paused {
+                None
+            } else {
+                let moment = Local::now();
+                let lap = self.read_lap_elapsed(moment);
+                self.lap_moments.push(moment);
+                self.laps.push(lap);
+                self.lap_elapsed = Duration::zero();
+                Some(lap)
+            }
+        }
         pub fn read(&self) -> Duration {
             if self.paused {
                 self.elapsed
             } else {
-                self.elapsed + (Local::now() - self.last_update)
+                self.elapsed + (Local::now() - self.last_start())
             }
+        }
+        fn read_lap_elapsed(&self, moment: DateTime<Local>) -> Duration {
+            self.lap_elapsed
+                + if self.lap_elapsed == Duration::zero()
+                    && !self.lap_moments.is_empty()
+                {
+                    moment - self.last_lap()
+                } else {
+                    moment - self.last_start()
+                }
         }
 
         /// Sets a callback to be used when `<Enter>` is pressed.
@@ -75,20 +124,29 @@ mod Timer {
             self.on_stop = Some(Rc::new(move |s, t| {
                 cb(s, t);
             }));
+            unimplemented!();
         }
 
-        fn stop(&mut self) -> EventResult {
-            self.pause();
-            let cb = self.on_stop.clone().unwrap();
-            // We return a Callback Rc<|s| cb(s, &*v)>
-            EventResult::Consumed(Some(Callback::from_fn(move |s| {
-                cb(s, self.elapsed)
-            })))
-        }
+        // fn stop(&mut self) -> EventResult {
+        //     self.pause();
+        //     let cb = self.on_stop.clone().unwrap();
+        //     // We return a Callback Rc<|s| cb(s, &*v)>
+        //     EventResult::Consumed(Some(Callback::from_fn(move |s| {
+        //         cb(s, self.elapsed)
+        //     })))
+        // }
     }
     impl View for TimerView {
         fn draw(&self, printer: &Printer) {
             printer.print((0, 0), &pretty(self.read()));
+            for i in 0..self.laps.len() {
+                printer.print((0, i + 1), &pretty(self.laps[i]));
+            }
+        }
+
+        fn required_size(&mut self, constraint: Vec2) -> Vec2 {
+            let _ = constraint;
+            Vec2::new(8, 8)
         }
 
         fn on_event(&mut self, event: Event) -> EventResult {
@@ -97,8 +155,11 @@ mod Timer {
                 Event::Char(' ') => {
                     self.pause_or_resume();
                 }
-                Event::Key(Key::Enter) if self.on_stop.is_some() => {
-                    return self.stop();
+                // Event::Key(Key::Enter) if self.on_stop.is_some() => {
+                //     return self.stop();
+                // }
+                Event::Char('l') => {
+                    self.lap();
                 }
                 _ => return EventResult::Ignored,
             }
