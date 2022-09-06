5cfddb6d9898ebcd11e64385d7442d0a75ea8b88
Alexandre Bury <alexandre.bury@gmail.com>
2022-09-06 10:47:23 -0400

    Fix example

--- cursive/examples/ansi.rs
+++ cursive/examples/ansi.rs
@@ -1,7 +1,15 @@
 fn main() {
+    // Start with some text content that includes ANSI codes.
+    // Often this could be the output of another command meant for humans.
     let content = include_str!("text_with_ansi_codes.txt");
+
+    // Parse the content as ANSI-decorated text.
     let styled = cursive::utils::markup::ansi::parse(content);
+
+    // Just give this to `TextView`
     let text_view = cursive::views::TextView::new(styled);
+
+    // And make a minimal app around that.
     let mut siv = cursive::default();
     siv.add_layer(text_view);
     siv.run();
