b039df2bc491a34734c856312441c90d9cb486fa
Tim Dubbins <57918941+timdubbins@users.noreply.github.com>
2023-07-28 21:03:39 +0100

    Add method to return character from char event.

--- cursive-core/src/event.rs
+++ cursive-core/src/event.rs
@@ -533,6 +533,15 @@ pub enum Event {
 }
 
 impl Event {
+    /// Returns the character, if `self` is a char event.
+    pub fn char(&self) -> Option<char> {
+        if let Event::Char(c) = *self {
+            Some(c)
+        } else {
+            None
+        }
+    }
+    
     /// Returns the position of the mouse, if `self` is a mouse event.
     pub fn mouse_position(&self) -> Option<Vec2> {
         if let Event::Mouse { position, .. } = *self {
@@ -542,7 +551,7 @@ impl Event {
         }
     }
 
-    /// Returns a mutable reference to the position of the mouse/
+    /// Returns a mutable reference to the position of the mouse.
     ///
     /// Returns `None` if `self` is not a mouse event.
     pub fn mouse_position_mut(&mut self) -> Option<&mut Vec2> {
