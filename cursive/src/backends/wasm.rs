12d7dca3538d333e0851778c7e2f70e809be1bb3
hyejin lee <genie4cs@gmail.com>
2023-08-27 14:53:19 +0900

    wrap writer logic on wasm backend

--- cursive/src/backends/wasm.rs
+++ cursive/src/backends/wasm.rs
@@ -1,26 +1,20 @@
 #![cfg(feature = "wasm-backend")]
 
-use cursive_core::{
-    event::{ Event, Key },
-    Vec2,
-    theme,
-};
-use std::collections::VecDeque;
-use std::rc::Rc;
-use std::cell::RefCell;
-use web_sys::HtmlCanvasElement;
 use wasm_bindgen::prelude::*;
 use crate::backend;
+
+const BUFFER_WIDTH: usize = 100;
+const BUFFER_HEIGHT: usize = 100;
 
 #[wasm_bindgen]
 #[derive(Debug, PartialEq)]
 #[repr(C)]
-struct TextColorPair  {
+struct ScreenChar  {
     text: char,
     color: ColorPair,
 }
 
-impl TextColorPair {
+impl ScreenChar {
     pub fn new(text: char, color: ColorPair) -> Self {
         Self {
             text,
@@ -29,16 +23,7 @@ impl TextColorPair {
     }
 }
 
-fn text_color_pairs_to_bytes(buffer: &Vec<TextColorPair>) -> &[u8] {
-    unsafe {
-        std::slice::from_raw_parts(
-            buffer.as_ptr() as *const u8,
-            buffer.len() * std::mem::size_of::<TextColorPair>(),
-        )
-    }
-}
-
-impl Clone for TextColorPair {
+impl Clone for ScreenChar {
     fn clone(&self) -> Self {
         Self {
             text: self.text,
@@ -47,40 +32,173 @@ impl Clone for TextColorPair {
     }
 }
 
-const WIDTH: usize = 100;
-const HEIGHT: usize = 100;
+#[repr(transparent)]
+struct Buffer {
+    chars: Vec<ScreenChar>,
+}
+
+impl Buffer {
+    pub fn new(color: ColorPair) -> Self {
+        Self {
+            chars: vec![ScreenChar::new(' ', color.clone()); BUFFER_WIDTH * BUFFER_HEIGHT],
+        }
+    }
+
+    pub fn set(self: &mut Buffer, index: usize, screen_char: ScreenChar) {
+        self.chars[index] = screen_char;
+    }
+}
+
+struct Writer {
+    buffer: Buffer,
+    color: ColorPair,
+}
+
+impl Writer {
+    pub fn new() -> Self {
+        let color = terminal_default_color_pair();
+        Self {
+            buffer: Buffer::new(color.clone()),
+            color,
+        }
+    }
+
+    pub fn write(self: &mut Writer, pos: Vec2, text: &str) {
+        for (i, c) in text.chars().enumerate() {
+            let x = pos.x + i;
+            self.buffer.set(BUFFER_WIDTH * pos.y + x, ScreenChar::new(c, self.color.clone()));
+        }
+    }
+
+    pub fn set_color(self: &mut Writer, color: ColorPair) {
+        self.color = color;
+    }
+
+    pub fn clear(self: &mut Writer, color: Color) {
+        let screen_char = ScreenChar::new(' ', ColorPair::new(color, color));
+
+        self.buffer
+            .chars
+            .iter_mut()
+            .for_each(|c| *c = screen_char.clone());
+    }
+
+    pub fn buffer(self: &Writer) -> &Vec<ScreenChar> {
+        &self.buffer.chars
+    }
+}
+
+/// Type of hex color which is r,g,b
+#[wasm_bindgen]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[repr(C)]
+struct Color {
+    red: u8, 
+    green: u8,
+    blue: u8
+}
+
+impl Color {
+    /// Creates a new `Color` with the given red, green, and blue values.
+    pub fn new(red: u8, green: u8, blue: u8) -> Self {
+        Self {
+            red,
+            green,
+            blue,
+        }
+    }
+}
+
+use cursive_core::theme::{ Color as CColor, BaseColor as CBaseColor };
+
+/// Convert cursive color to hex color.
+fn cursive_to_color(color: CColor) -> Color {
+    match color {
+        CColor::Dark(CBaseColor::Black) => Color::new(0,0,0),
+        CColor::Dark(CBaseColor::Red) => Color::new(128,0,0),
+        CColor::Dark(CBaseColor::Green) => Color::new(0,128,0),
+        CColor::Dark(CBaseColor::Yellow) => Color::new(128,128,0),
+        CColor::Dark(CBaseColor::Blue) => Color::new(0,0,128),
+        CColor::Dark(CBaseColor::Magenta) => Color::new(128,0,128),
+        CColor::Dark(CBaseColor::Cyan) => Color::new(0,128,128),
+        CColor::Dark(CBaseColor::White) => Color::new(182,182,182),
+        CColor::Light(CBaseColor::Black) => Color::new(128,128,128),
+        CColor::Light(CBaseColor::Red) => Color::new(255,0,0),
+        CColor::Light(CBaseColor::Green) => Color::new(0,255, 0),
+        CColor::Light(CBaseColor::Yellow) => Color::new(255,255,0),
+        CColor::Light(CBaseColor::Blue) => Color::new(0,0,255),
+        CColor::Light(CBaseColor::Magenta) => Color::new(255,0,255),
+        CColor::Light(CBaseColor::Cyan) => Color::new(0,255,255),
+        CColor::Light(CBaseColor::White) => Color::new(255,255,255),
+        CColor::Rgb(r, g, b) =>  Color::new(r,g,b),
+        CColor::RgbLowRes(r,g ,b ) =>  Color::new(r,g,b),
+        CColor::TerminalDefault =>  Color::new(0,255,0),
+    }
+}
+
+/// Type of color pair.
+#[derive(Clone, Debug, PartialEq, Eq)]
+#[repr(C)]
+struct ColorPair {
+    /// Foreground text color.
+    pub front: Color,
+    /// Background color.
+    pub back: Color,
+}
+
+impl ColorPair {
+    /// Creates a new `ColorPair` with the given foreground and background colors.
+    pub fn new(front: Color, back: Color) -> Self {
+        Self {
+            front,
+            back,
+        }
+    }
+}
+
+use cursive_core::theme::ColorPair as CColorPair;
+/// Convert cursive color pair to hex color pair.
+fn cursive_to_color_pair(c: CColorPair) -> ColorPair {
+    ColorPair {
+        front: cursive_to_color(c.front),
+        back: cursive_to_color(c.back),
+    }
+}
+
+fn terminal_default_color_pair() -> ColorPair {
+    cursive_to_color_pair(CColorPair {
+    front: CColor::Light(CBaseColor::Black),
+    back:CColor::Dark(CBaseColor::Green),
+    })
+}
 
 #[wasm_bindgen(module = "/src/backends/canvas.js")]
 extern "C" {
     fn paint(buffer: &[u8]);
 }
 
+use std::collections::VecDeque;
+use std::rc::Rc;
+use std::cell::RefCell;
+use web_sys::{ Document, HtmlCanvasElement };
+
 /// Backend using wasm.
 pub struct Backend {
     canvas: HtmlCanvasElement,
-    color: RefCell<ColorPair>,
     events: Rc<RefCell<VecDeque<Event>>>,
-    buffer: RefCell<Vec<TextColorPair>>,
+    writer: RefCell<Writer>,
 }
+
+use cursive_core::{
+    event::{ Event, Key },
+    Vec2,
+    theme::Effect,
+};
+
 impl Backend {
     /// Creates a new Cursive root using a wasm backend and given HTML canvas.
     pub fn new(canvas: HtmlCanvasElement) -> std::io::Result<Box<dyn backend::Backend>> {
-        let document = web_sys::window()
-            .ok_or(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                "Failed to get window",
-            ))?
-            .document()
-            .ok_or(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                "Failed to get document",
-            ))?;
-
-        let color = cursive_to_color_pair(theme::ColorPair {
-            front: theme::Color::Light(theme::BaseColor::Black),
-            back:theme::Color::Dark(theme::BaseColor::Green),
-        });
-
+        let document = Self::document()?;
         let events = Rc::new(RefCell::new(VecDeque::new()));
         let cloned = events.clone();
         let closure = Closure::wrap(Box::new(move |event: web_sys::KeyboardEvent| {
@@ -95,21 +213,26 @@ impl Backend {
                 "Failed to add event listener",
             ))?;
         closure.forget();
-
-        let buffer = vec![TextColorPair::new(' ', color.clone()); WIDTH * HEIGHT];
         
         let c = Backend {
             canvas,
-            color: RefCell::new(color),
             events,
-            buffer: RefCell::new(buffer),
+            writer: RefCell::new(Writer::new()),
         };
         Ok(Box::new(c))
     }
 
     /// Creates a new Cursive root using a wasm backend.
     pub fn init() -> std::io::Result<Box<dyn backend::Backend>> {
-        let document = web_sys::window()
+        let canvas = Self::canvas()?;
+        canvas.set_width(1000);
+        canvas.set_height(1000);
+
+        Self::new(canvas)
+    }
+
+    fn document() -> Result<Document, std::io::Error> {
+        web_sys::window()
             .ok_or(std::io::Error::new(
                 std::io::ErrorKind::Other,
                 "Failed to get window",
@@ -118,9 +241,12 @@ impl Backend {
             .ok_or(std::io::Error::new(
                 std::io::ErrorKind::Other,
                 "Failed to get document",
-            ))?;
+            ))
+    }
 
-        let canvas = document.get_element_by_id("cursive-wasm-canvas")
+    fn canvas() -> Result<HtmlCanvasElement, std::io::Error> {
+        Self::document()?
+            .get_element_by_id("cursive-wasm-canvas")
             .ok_or(std::io::Error::new(
                 std::io::ErrorKind::Other,
                 "Failed to get window",
@@ -129,11 +255,11 @@ impl Backend {
             .map_err(|_| std::io::Error::new(
                 std::io::ErrorKind::Other,
                 "Failed to cast canvas",
-            ))?;
-        canvas.set_width(1000);
-        canvas.set_height(1000);
+            ))
+    }
 
-        Self::new(canvas)
+    fn terminal_default_color() -> ColorPair {
+        terminal_default_color_pair()
     }
 
     fn to_cursive_event(event: web_sys::KeyboardEvent) -> Event {
@@ -173,6 +299,17 @@ impl Backend {
             }  
         }
     }
+
+    fn text_color_pairs_to_bytes(self: &Backend) -> &[u8] {
+        let binding = self.writer.borrow();
+        let data = binding.buffer();
+        unsafe {
+            std::slice::from_raw_parts(
+                data.as_ptr() as *const u8,
+                data.len() * std::mem::size_of::<ScreenChar>(),
+            )
+        }
+    }
 }
 
 impl cursive_core::backend::Backend for Backend {
@@ -185,8 +322,7 @@ impl cursive_core::backend::Backend for Backend {
     }
 
     fn refresh(self: &mut Backend) {
-        let data = self.buffer.borrow().clone();
-        paint(text_color_pairs_to_bytes(&data));
+        paint(self.text_color_pairs_to_bytes());
     }
 
     fn has_colors(self: &Backend) -> bool {
@@ -194,106 +330,29 @@ impl cursive_core::backend::Backend for Backend {
     }
 
     fn screen_size(self: &Backend) -> Vec2 {
-        Vec2::new(WIDTH, HEIGHT)
+        Vec2::new(BUFFER_WIDTH, BUFFER_HEIGHT)
     }
 
     fn print_at(self: &Backend, pos: Vec2, text: &str) {
-        let color = (*self.color.borrow()).clone();
-        let mut buffer = self.buffer.borrow_mut();
-        for (i, c) in text.chars().enumerate() {
-            let x = pos.x + i;
-            buffer[WIDTH * pos.y + x] = TextColorPair::new(c, color.clone());
-        }
+        self.writer.borrow_mut().write(pos, text);
     }
 
-    fn clear(self: &Backend, color: cursive_core::theme::Color) {
-        let color = cursive_to_color(color);
-        let pair = ColorPair {
-            front: color,
-            back: color,
-        };
-
-        self.buffer
-        .borrow_mut()
-        .fill(TextColorPair::new(' ', pair.clone()));
+    fn clear(self: &Backend, color: CColor) {
+        self.writer.borrow_mut().clear(cursive_to_color(color))
     }
 
-    fn set_color(self: &Backend, color_pair: cursive_core::theme::ColorPair) -> cursive_core::theme::ColorPair {
-        let mut color = self.color.borrow_mut();
-        *color = cursive_to_color_pair(color_pair);
+    fn set_color(self: &Backend, color_pair: CColorPair) -> CColorPair {
+        self.writer.borrow_mut().set_color(cursive_to_color_pair(color_pair));
         color_pair
     }
 
-    fn set_effect(self: &Backend, _: cursive_core::theme::Effect) {
+    fn set_effect(self: &Backend, _: Effect) {
     }
 
-    fn unset_effect(self: &Backend, _: cursive_core::theme::Effect) {
+    fn unset_effect(self: &Backend, _: Effect) {
     }
 
     fn name(&self) -> &str {
         "cursive-wasm-backend"
-    }
-}
-
-
-/// Type of hex color which is r,g,b
-#[wasm_bindgen]
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub struct Color {
-    red: u8, 
-    green: u8,
-    blue: u8
-}
-
-impl Color {
-    /// Creates a new `Color` with the given red, green, and blue values.
-    pub fn new(red: u8, green: u8, blue: u8) -> Self {
-        Self {
-            red,
-            green,
-            blue,
-        }
-    }
-}
-
-/// Type of color pair.
-#[derive(Clone, Debug, PartialEq, Eq)] 
-pub struct ColorPair {
-    /// Foreground text color.
-    pub front: Color,
-    /// Background color.
-    pub back: Color,
-}
-
-/// Convert cursive color to hex color.
-pub fn cursive_to_color(color: theme::Color) -> Color {
-    match color {
-        theme::Color::Dark(theme::BaseColor::Black) => Color::new(0,0,0),
-        theme::Color::Dark(theme::BaseColor::Red) => Color::new(128,0,0),
-        theme::Color::Dark(theme::BaseColor::Green) => Color::new(0,128,0),
-        theme::Color::Dark(theme::BaseColor::Yellow) => Color::new(128,128,0),
-        theme::Color::Dark(theme::BaseColor::Blue) => Color::new(0,0,128),
-        theme::Color::Dark(theme::BaseColor::Magenta) => Color::new(128,0,128),
-        theme::Color::Dark(theme::BaseColor::Cyan) => Color::new(0,128,128),
-        theme::Color::Dark(theme::BaseColor::White) => Color::new(182,182,182),
-        theme::Color::Light(theme::BaseColor::Black) => Color::new(128,128,128),
-        theme::Color::Light(theme::BaseColor::Red) => Color::new(255,0,0),
-        theme::Color::Light(theme::BaseColor::Green) => Color::new(0,255, 0),
-        theme::Color::Light(theme::BaseColor::Yellow) => Color::new(255,255,0),
-        theme::Color::Light(theme::BaseColor::Blue) => Color::new(0,0,255),
-        theme::Color::Light(theme::BaseColor::Magenta) => Color::new(255,0,255),
-        theme::Color::Light(theme::BaseColor::Cyan) => Color::new(0,255,255),
-        theme::Color::Light(theme::BaseColor::White) => Color::new(255,255,255),
-        theme::Color::Rgb(r, g, b) =>  Color::new(r,g,b),
-        theme::Color::RgbLowRes(r,g ,b ) =>  Color::new(r,g,b),
-        theme::Color::TerminalDefault =>  Color::new(0,255,0),
-    }
-}
-
-/// Convert cursive color pair to hex color pair.
-pub fn cursive_to_color_pair(c: theme::ColorPair) -> ColorPair {
-    ColorPair {
-        front: cursive_to_color(c.front),
-        back: cursive_to_color(c.back),
     }
 }
