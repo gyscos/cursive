afed13c03ac1f5d1ed62a6f9892408b9709d9d72
Alexandre Bury <alexandre.bury@gmail.com>
2024-06-05 10:10:16 -0400

    wip

--- cursive-core/src/theme/effect.rs
+++ cursive-core/src/theme/effect.rs
@@ -1,9 +1,10 @@
-use enumset::EnumSetType;
+use enum_map::{Enum, EnumMap};
+use enumset::{EnumSet, EnumSetType};
 use std::str::FromStr;
 
 /// Text effect
 #[allow(clippy::derived_hash_with_manual_eq)] // We do derive it through EnumSetType
-#[derive(EnumSetType, Debug, Hash)]
+#[derive(EnumSetType, Enum, Debug, Hash)]
 pub enum Effect {
     /// No effect
     Simple,
@@ -28,6 +29,50 @@ pub enum Effect {
 
     /// Foreground text blinks (background color is static).
     Blink,
+}
+
+/// A set of effects status.
+///
+/// Describes what to do for each effect: enable, disable, preserve, xor.
+pub struct Effects(pub EnumMap<Effect, EffectStatus>);
+
+impl Effects {
+    /// Resolve an effects directive into concrete effects.
+    pub fn resolve(&self, old: ConcreteEffects) -> ConcreteEffects {
+        let mut result = ConcreteEffects::default();
+        for (effect, status) in self.0 {
+            if matches!(
+                (status, old.contains(effect)),
+                (EffectStatus::On, _)
+                    | (EffectStatus::InheritParent, true)
+                    | (EffectStatus::OppositeParent, false)
+            ) {
+                result.insert(effect);
+            }
+        }
+        result
+    }
+}
+
+/// A concrete set of effects to enable.
+///
+/// Every missing effect should be disabled.
+pub type ConcreteEffects = EnumSet<Effect>;
+
+/// Describes what to do with an effect.
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum EffectStatus {
+    /// Force the effect on, regardless of the parent.
+    On,
+
+    /// Force the effect off, regardless of the parent.
+    Off,
+
+    /// Keep the same effect status as the parent.
+    InheritParent,
+
+    /// Use the opposite state from the parent.
+    OppositeParent,
 }
 
 impl FromStr for Effect {
